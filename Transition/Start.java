c99649b335aa7c9517549ca55cb56e1a51a834ea
chidodou <dodomail8408@gmail.com>
2025-01-16 13:50:33 -0600

    test

---
+++ Transition/Start.java
@@ -0,0 +1,8 @@
+package Transition;
+
+public class Start {
+    public static void main(String[] args) {
+        TransitionGame transition = new TransitionGame();
+        transition.start(); // start app
+    }
+}
