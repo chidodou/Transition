42b053b86351f32f984592c41ea0fd3bb932e210
chidodou <dodomail8408@gmail.com>
2025-04-29 13:37:24 -0500

    handling beatmaps, audio, note/object loading, notepad editing; editor should come last

--- rootTransition/Note.java
+++ rootTransition/Note.java
@@ -2,18 +2,22 @@ public class Note {
     // public class for all general notes, disregarding types
     // will be addressed later
     public float time;   // in ms
-    public int position;   // which track/lane
+    public int position, type;   // which track/lane
     public boolean hit = false; // detector for clicked objects
 
-    public Note(float time, int lane) {
+    public Note(float time, int lane, int type) {
+        // time: at what position in milliseconds
+        // position: x/y coordinate on the screen
+        //type: if note is click or no click
     }
 
     public void hitNote() {
-
-
+        // define to hit a note
     }
     public void note() {
-
+        // define the appearing of a note as a renderable object, how it interacts with the editor, how it
     }
+
+    // this one should be simple
     public boolean isHit() { return hit = true;}
 }
